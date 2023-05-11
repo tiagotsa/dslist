56c2392e4ec738b7ffd6b18cd84ec951a4b910eb
Tiago Almeida <tiago.almeidat@outlook.com>
2023-05-11 10:50:09 -0300

    CORS config2

--- src/main/java/com/devsuperior/dslist/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -2,10 +2,13 @@ package com.devsuperior.dslist.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig {
+
 	@Value("${cors.origins}")
 	private String corsOrigins;
 	
@@ -18,5 +21,5 @@ public class WebConfig {
 			}
 		};
 	}
-
+	
 }
